f3fb6136f8a5b4a1ca2038937da051d6f044ce82
Vasan <vasansoundararajan.21@gmail.com>
2024-11-07 22:19:46 +0530

    exno6

---
+++ app/src/main/java/com/example/ex06/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.ex06;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageView img;
+    Button bt1,bt2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt1 = (Button)findViewById(R.id.button);
+        bt2= (Button) findViewById(R.id.button2);
+        img = (ImageView)findViewById(R.id.imageView);
+        bt1.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        img.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                img.setImageResource(R.drawable.img);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+        bt2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        img.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                img.setImageResource(R.drawable.img_1);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+    }
+}
